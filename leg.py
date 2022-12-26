5858f98b296d98181873317b1f464dde4cef9032
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-12-26 18:47:31 +0000

    Create leg.py

---
+++ leg.py
@@ -0,0 +1,15 @@
+"""
+Leg related processes and functions go here
+Classes:
+    Leg
+"""
+
+class Leg():
+    """
+    Legs objects
+    """
+
+    def __init__(self):
+        self.hip_pos = (0,0)
+        self.knee_pos = (0, 75)
+        self.foot_pos = (0, 150)
