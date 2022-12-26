f71902c91243fef8409635aa54a540ad4a0e11b1
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-12-26 18:47:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ A 2D simulation involving forces of a Quadruped's leg
 """
 
 import pygame
+from leg import Leg
 
+####################
+# Size definitions #
+####################
+DIAMETER = 50
+LENGTH = 200
+
+###################
+# GUI definitions #
+###################
 FPS = 60
 WIDTH, HEIGHT = 900, 900
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Dog leg simulation")
 
-#ARROW = pygame.image.load("./Assets/arrow.png")
-#LINE = pygame.image.load("./Assets/line.png")
-#CIRCLE = pygame.image.load("./Assets/circle.png")
+#####################
+# Shape definitions #
+#####################
+ARROW = pygame.image.load("./Assets/arrow.png")
+LINE = pygame.image.load("./Assets/line.png")
+CIRCLE = pygame.image.load("./Assets/circle.png")
+
+my_leg = Leg()
 
 def show_gui():
     """
@@ -22,12 +37,15 @@ def show_gui():
     run = True
 
     while run:
+        WIN.fill("white")
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         pygame.display.update()
         clock.tick(FPS)
+
     pygame.quit()
 
 if __name__ == "__main__":
