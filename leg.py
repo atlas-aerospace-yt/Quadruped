61ab384563f8122d019d12d34ec20cc019ad53e2
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-12-27 17:00:27 +0000

    Update leg.py

--- leg.py
+++ leg.py
@@ -10,15 +10,16 @@ import pygame
 ##############
 # Conversion #
 ##############
-METERS_TO_PIXELS = 3_333
+METERS_TO_PIXELS = 3_000
 
 ####################
 # Size definitions #
 ####################
-DIAMETER = 0.015 * METERS_TO_PIXELS
-LENGTH = 0.075 * METERS_TO_PIXELS
-WIDTH = 0.005 * METERS_TO_PIXELS
+LENGTH = 0.075
+WIDTH = 0.005
 MASS = 1
+TORQUE = 0.137
+
 
 #####################
 # Shape definitions #
@@ -34,35 +35,54 @@ class Leg():
     ALL ANGLES ARE TO BE DEFINED AS BEARINGS POINTING NORTH
     """
 
-    def __init__(self, theta_one=135, theta_two=180):
+    def __init__(self, theta_one=180, theta_two=180):
 
         # joint definitions
-        self.foot_pos = (0, 549)
-        self.knee_pos = self.get_co_ordinate(self.foot_pos, LENGTH, theta_one)
-        self.hip_pos = self.get_co_ordinate(self.knee_pos, LENGTH, theta_two)
+        self.hip_pos = (0, 0)
+        self.knee_pos = self.get_co_ordinate(self.hip_pos, LENGTH, theta_one)
+        self.foot_pos = self.get_co_ordinate(self.knee_pos, LENGTH, theta_two)
 
         # physics definitions
         self.position = [0, 0]
         self.velocity = [0, 0]
         self.acceleration = [0, 9.81]
 
+    def get_resultant_forces(self, theta_one:'Degrees', theta_two:'Degrees')->'Newtons':
+        """
+        Calculates the X and Y component of the resultant forces
+
+        Args:
+            theta_one (Degrees): the bearing of the knee from the foot
+            theta_two (Degrees): the bearing of the hip from the knee
+
+        Returns:
+            Newtons: The force vector output
+        """
+        weight = 9.81 * MASS
+        force_one_x = np.sin((theta_one-90)*np.pi/180) * TORQUE / LENGTH
+        force_one_y = np.cos((theta_one-90) * np.pi/180) * TORQUE / LENGTH
+
+        force_y = force_one_y - weight
+
+        return (force_one_x, force_y)
+
     def get_position(self, delta_time):
         """
-        Calculates the position of the foot
+        Calculates the position of the hip
 
         Args:
             delta_time (float): change in time
 
         Returns:
-            list: the position of the foot
+            list: the position of the hip
         """
         self.velocity[1] += self.acceleration[1] * delta_time
         self.position[1] += self.velocity[1] * delta_time
 
-        # checks if the floor has been hit
-        if self.position[1] * METERS_TO_PIXELS > 549:
-            self.position[1] = 549 /  METERS_TO_PIXELS
-        if self.position[1] * METERS_TO_PIXELS == 549:
+        # checks if the floor has been hipt
+        if self.position[1] > 0.2:
+            self.position[1] = 0.2
+        if self.position[1] == 0.2:
             self.velocity[1] = 0
 
         return self.position
@@ -72,20 +92,20 @@ class Leg():
         Sets the angle leg and re calculates the coordinates
 
         Args:
-            theta_one (int): the bearing of the knee from the foot
-            theta_two (int): the bearing of the hip from the knee
+            theta_one (int): the bearing of the knee from the hip
+            theta_two (int): the bearing of the foot from the knee
         """
-        self.knee_pos = self.get_co_ordinate(self.foot_pos, LENGTH, theta_one)
-        self.hip_pos = self.get_co_ordinate(self.knee_pos, LENGTH, theta_two)
+        self.knee_pos = self.get_co_ordinate(self.hip_pos, LENGTH, theta_one)
+        self.foot_pos = self.get_co_ordinate(self.knee_pos, LENGTH, theta_two)
 
-    def set_foot_pos(self, foot_pos):
+    def set_hip_pos(self, hip_pos):
         """
-        Sets the foot position using x and y coordinates
+        Sets the hip position using x and y coordinates
 
         Args:
-            foot_pos (list): the coordinates of the foot
+            hip_pos (list): the coordinates of the hip
         """
-        self.foot_pos = (foot_pos[0]  * METERS_TO_PIXELS, foot_pos[1] * METERS_TO_PIXELS)
+        self.hip_pos = (hip_pos[0], hip_pos[1])
 
     def get_co_ordinate(self, origin, distance, bearing):
         """
@@ -103,27 +123,6 @@ class Leg():
         y_co_ord = origin[1] + np.cos(bearing * np.pi/180) * distance
         return (int(x_co_ord), int(y_co_ord))
 
-
-class LegImage(Leg):
-    """
-    Generates and transforms the images to draw the leg
-
-    Inheritance:
-        Leg : The leg object that needs to be displayed
-    """
-
-    def __init__(self):
-        # Angle definitions
-        self.theta_one = 180
-        self.theta_two = 180
-
-        super().__init__(self.theta_one, self.theta_two)
-
-        # Pivot definitions
-        self.foot = pygame.transform.scale(CIRCLE, (DIAMETER, DIAMETER))
-        self.thigh_joint = pygame.transform.scale(CIRCLE, (DIAMETER, DIAMETER))
-        self.hip_joint = pygame.transform.scale(CIRCLE, (DIAMETER, DIAMETER))
-
     def get_length(self, co_ord_one, cor_ord_two):
         """
         calculates the distance between two points
@@ -139,24 +138,3 @@ class LegImage(Leg):
         y_distance = co_ord_one[1] - cor_ord_two[1]
 
         return int(np.sqrt(x_distance ** 2 + y_distance ** 2))
-
-    def display_leg(self, window):
-        """
-        blits the leg onto the window
-
-        Args:
-            window (pygame.display): the gui
-        """
-        self.set_foot_pos(self.get_position(0.02))
-
-        window.blit(self.foot, (self.foot_pos[0] + 50, self.foot_pos[1] + 50))
-        window.blit(self.thigh_joint, (self.knee_pos[0] + 50, self.knee_pos[1] + 50))
-        window.blit(self.hip_joint, (self.hip_pos[0] + 50, self.hip_pos[1] + 50))
-
-        pygame.draw.line(window, "black", (self.foot_pos[0] + 75, self.foot_pos[1] + 75),
-                         (self.knee_pos[0] + 75, self.knee_pos[1] + 75))
-        pygame.draw.line(window, "black", (self.knee_pos[0] + 75, self.knee_pos[1] + 75),
-                         (self.hip_pos[0] + 75, self.hip_pos[1] + 75))
-
-        # draw the floor
-        pygame.draw.line(window, "black", (0, 650), (500, 650))
