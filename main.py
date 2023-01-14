bb3a08b1893ce6df25074bc51e746b6c4631e90f
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-01-14 01:04:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from shapes import Circle
 # Constant joint radius
 RADIUS = 0.01
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 5))
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 5))
 
 ax.set_aspect('equal', adjustable='box')
 
@@ -19,10 +19,10 @@ class GraphLeg(Leg):
     """
     Graphs the leg
     """
-    def __init__(self):
+    def __init__(self, theta_one, theta_two):
         self.delta_time = 0.001
 
-        super().__init__((180, 180), (120, 240))
+        super().__init__((theta_one, theta_two), (120, 240))
 
     def plot_joints(self, axes):
         """
@@ -36,9 +36,9 @@ class GraphLeg(Leg):
         knee_joint = Circle(RADIUS, self.knee_pos, 0.0001)
         foot = Circle(RADIUS, self.foot_pos, 0.0001)
 
-        axes.plot(hip_joint.x_list, hip_joint.y_list, color="blue")
-        axes.plot(knee_joint.x_list, knee_joint.y_list, color="blue")
-        axes.plot(foot.x_list, foot.y_list, color="blue")
+        axes.plot(hip_joint.x_list, hip_joint.y_list, color="black")
+        axes.plot(knee_joint.x_list, knee_joint.y_list, color="black")
+        axes.plot(foot.x_list, foot.y_list, color="black")
 
     def plot_leg(self, axes):
         """
@@ -103,10 +103,11 @@ class GraphLeg(Leg):
         self.plot_leg(ax)
         self.plot_forces(ax, forces)
 
-        plt.text(-0.1, 0.1, f"time: {round(self.elapsed, 2)}s", fontsize=14)
+        # plt.text(-0.1, 0.1, f"time: {round(self.elapsed, 2)}s", fontsize=14)
 
-my_leg = GraphLeg()
+my_leg = GraphLeg(150, 210)
 anim = FuncAnimation(fig, my_leg.animate, frames=121, interval=0.01)
+# my_leg.animate(0)
 
-#anim.save("leg_moving.gif", writer='A. C. Armitage')
+# anim.save("leg_moving.gif", writer='A. C. Armitage')
 plt.show()
