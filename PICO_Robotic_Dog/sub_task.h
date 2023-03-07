0d58d689f99bfd6d0682b0284e4f469fa5e1b96e
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-03-07 22:40:02 +0000

    Create sub_task.h

---
+++ PICO_Robotic_Dog/sub_task.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "utility.h"
+
+#define LENGTH 0.075f
+#define GEAR_RATIO 3/2
+#define GEAR_RATIO_RECIPROCAL 2/3
+
+namespace subtask {
+
+  float get_alpha(float x, float y)
+  {
+    float length_new = 0.5f * sqrt(sq(x) + sq(y));
+    return acos(length_new/LENGTH) * 180.0f/PI;
+  }
+
+  float get_beta(float alpha)
+  {
+    return 180 - 2 * alpha;
+  }
+
+  float get_theta(float x, float y)
+  {
+    return atan(x/y) * 180.0f/PI;
+  }
+
+  // wrapper function to get the hip_output
+  float get_hip_output(float x, float y)
+  {
+    float alpha = get_alpha(x, y);
+    float theta = get_theta(x, y);
+
+    return GEAR_RATIO * (alpha + theta);
+  }
+
+  // wrapper function to get the knee_output
+  float get_knee_output(float x, float y)
+  {
+    float alpha = get_alpha(x, y);
+    float beta = get_beta(alpha);
+    float theta = get_theta(x, y);
+    float u = (1 - GEAR_RATIO_RECIPROCAL) * (alpha + theta);
+
+    return GEAR_RATIO * (180 - (u + beta));
+  }
+}
