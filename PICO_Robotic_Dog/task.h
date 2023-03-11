39415757c0075ee6053f53f2d4989e4a2d662221
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-03-11 22:31:18 +0000

    Simple up and down demo

--- PICO_Robotic_Dog/task.h
+++ PICO_Robotic_Dog/task.h
@@ -6,26 +6,59 @@
 #include "utility.h"
 #include "sidekick.h"
 
+#define NUM_OF_LEGS 4
+
 namespace task
 {
-    void Setup()
-    {
-        actuators::init();
-    }
 
-    void ActuatorTest()
-    {
-    }
+  int values[8];
+  float hip;
+  float knee;
 
-    void Calibration()
-    {
-    }
+  void Setup()
+  {
+      actuators::init();
+  }
 
-    void Loop()
-    {
-      //float knee_value = subtask::get_knee_output(-0.04f, 0.12f);
-      //float hip_value = subtask::get_hip_output(-0.04f, 0.12f);
+  void ActuatorTest()
+  {
+  }
 
-      actuators::write();
+  void Calibration()
+  {
+  }
+
+  void Loop()
+  {
+    for (float height=0.09f; height < 0.15f; height+= 0.01f)
+    {
+      for (int i=0; i<8; i ++)
+      {
+        if (i%2 == 0){
+          values[i] = subtask::get_hip_output(0, height);
+        }
+        else
+        {
+          values[i] = subtask::get_knee_output(0, height);
+        }
+      }
+      actuators::write(values);
+      delay(200);
     }
+    for (float height=0.15f; height > 0.09f; height-= 0.01f)
+    {
+      for (int i=0; i<8; i ++)
+      {
+        if (i%2 == 0){
+          values[i] = subtask::get_hip_output(0, height);
+        }
+        else
+        {
+          values[i] = subtask::get_knee_output(0, height);
+        }
+      }
+      actuators::write(values);
+      delay(200);
+    }
+  }
 }
