9eb59c276ae0896241569e465453b7dc3e9c0999
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-03-12 20:47:12 +0000

    Added vertical motion.
    Demo of the dog moving horizontally and vertically.

--- PICO_Robotic_Dog/task.h
+++ PICO_Robotic_Dog/task.h
@@ -17,11 +17,12 @@ namespace task
 
   void Setup()
   {
-      actuators::init();
+    actuators::init();
   }
 
   void ActuatorTest()
   {
+    actuators::test();
   }
 
   void Calibration()
@@ -45,24 +46,60 @@ namespace task
       PRINT(width);
       PRINT(" , ");
       actuators::write(values);
-      delay(200);
+      delay(100);
     }
     for (int width=8; width>0; width--)
     {
       for (int i=0; i<8; i ++)
       {
         if (i%2 == 0){
-          values[i] = subtask::get_hip_output(width, 10);
+          values[i] = subtask::get_hip_output(width, 12);
         }
         else
         {
-          values[i] = subtask::get_knee_output(width, 10);
+          values[i] = subtask::get_knee_output(width, 12);
         }
       }
       PRINT(width);
       PRINT(" , ");
       actuators::write(values);
-      delay(200);
+      delay(100);
     }
+    delay(1000);
+    for (int height=15; height>8; height--)
+    {
+      for (int i=0; i<8; i ++)
+      {
+        if (i%2 == 0){
+          values[i] = subtask::get_hip_output(0, height);
+        }
+        else
+        {
+          values[i] = subtask::get_knee_output(0, height);
+        }
+      }
+      PRINT(height);
+      PRINT(" , ");
+      actuators::write(values);
+      delay(100);
+    }
+    for (int height=8; height<15; height++)
+    {
+      for (int i=0; i<8; i ++)
+      {
+        if (i%2 == 0){
+          values[i] = subtask::get_hip_output(0, height);
+        }
+        else
+        {
+          values[i] = subtask::get_knee_output(0, height);
+        }
+      }
+      PRINT(height);
+      PRINT(" , ");
+      actuators::write(values);
+      delay(100);
+    }
+    delay(1000);
   }
 }
