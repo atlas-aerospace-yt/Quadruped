bd3739b6e07c3ba2f43a548822573ce95e453b1b
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-03-17 23:20:02 +0000

    Update sub_task.h

--- PICO_Robotic_Dog/sub_task.h
+++ PICO_Robotic_Dog/sub_task.h
@@ -9,40 +9,6 @@ namespace subtask {
 
   /*
   *
-  * Gait and walking motion calculations are in this section
-  * of the code.
-  *
-  */
-
-  const int gait[] = {{0, 14}, {2, 14}, {4, 14}, {0, 10}};
-
-  int stats[] = {0, 2, 0, 2};
-  int positions[8];
-
-  void update_gait()
-  {
-    for (int i=0; i<sizeof(stats); i++)
-    {
-      stats[i] ++;
-      if (stats[i] > sizeof(gait)-1)
-      {
-        stats[i] = 0;
-      }
-    }
-  }
-
-  void get_positions()
-  {
-    for (int i=0; i<sizeof(stats); i++)
-    {
-      for (int j=0; j<sizeof(stats[i]); i++)
-      {
-
-      }
-    }
-  }
-  /*
-  *
   * Inverse kinematic calculations are in this section of
   * the code.
   *
@@ -66,24 +32,71 @@ namespace subtask {
   }
 
   // wrapper function to get the hip_output
-  float get_hip_output(float x, float y)
+  int get_hip_output(float x, float y)
   {
-    float alpha = get_alpha(-x, y);
-    float theta = get_theta(-x, y);
+    float alpha = get_alpha(x, y);
+    float theta = get_theta(x, y);
     float hip = GEAR_RATIO * (alpha + theta);
 
     return hip < 0 ? 0 : hip;
   }
 
   // wrapper function to get the knee_output
-  float get_knee_output(float x, float y)
+  int get_knee_output(float x, float y)
   {
-    float alpha = get_alpha(-x, y);
+    float alpha = get_alpha(x, y);
     float beta = get_beta(alpha);
-    float theta = get_theta(-x, y);
+    float theta = get_theta(x, y);
     float u = 180 - (1/2 * (theta + alpha) + beta);
     float knee = GEAR_RATIO * u;
 
     return knee < 0 ? 0 : knee;
+  }
+
+  /*
+  *
+  * Gait and walking motion calculations are in this section
+  * of the code.
+  *
+  */
+
+  int gait[][2] = {{-4, 14}, {0, 14}, {4, 14}, {0, 6}};
+  int stats[] = {2, 0, 0, 2};
+  int positions[8];
+
+  void update_gait()
+  {
+    for (int i=0; i<4; i++)
+    {
+      stats[i] ++;
+      if (stats[i] > 3)
+      {
+        stats[i] = 0;
+      }
+    }
+  }
+
+  void get_positions()
+  {
+    for (int i=0; i<4; i++)
+    {
+      int stat = stats[i];
+      PRINT("Gait: ")
+      PRINT(gait[stat][0]);
+      PRINT(" , ");
+      PRINT(gait[stat][1]);
+      PRINT(" , ");
+      PRINT("Servo: ")
+      PRINT(get_hip_output(gait[stat][0], gait[stat][1]));
+      PRINT(" , ")
+      PRINT(get_knee_output(gait[stat][0], gait[stat][1]));
+      PRINT(" , ")
+
+      int hip_indx = 2 * i;
+      int knee_indx = 2 * i + 1;
+
+      positions[hip_indx] = get_hip_output(gait[stat][0], gait[stat][1]);
+      positions[knee_indx] = get_knee_output(gait[stat][0], gait[stat][1]);
+    }
   }
 }
