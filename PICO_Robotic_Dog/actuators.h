ffcbdbe07aa454c0d944eac0f162519eb47d9d02
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-05-14 22:56:59 +0100

    New write functions

--- PICO_Robotic_Dog/actuators.h
+++ PICO_Robotic_Dog/actuators.h
@@ -10,7 +10,7 @@
 
 #include "actuator_struct.h"
 #include "utility.h"
-//#include "internal_defs.h"
+#include "sub_task.h"
 
 //-----Internals------
 
@@ -21,20 +21,23 @@
 //Include your Actuator libraries here
 #define MAX 180
 #define MIN 0
+#define LERP_PTS 100
 
 namespace actuators{
 
   // Globals can be defined here
-  int pins_bl[] = {15, 14};
-  int pins_br[] = {13, 12};
-  int pins_fl[] = {11, 10};
-  int pins_fr[] = {9, 8};
+  int pins_fr[] = {15, 14};
+  int pins_fl[] = {13, 12};
+  int pins_br[] = {11, 10};
+  int pins_bl[] = {9, 8};
 
   // SideKick actuator_struct definitions
   ActuatorGroup<Servo,2> leg_bl;
   ActuatorGroup<Servo,2> leg_br;
   ActuatorGroup<Servo,2> leg_fl;
   ActuatorGroup<Servo,2> leg_fr;
+
+  //Lerp(0, 180);
 
   void init()
   {
@@ -49,52 +52,52 @@ namespace actuators{
     int pos_forward[] = {0, 180};
     int pos_backward[] = {180, 0};
 
-    leg_bl.write(pos_backward);
-    leg_br.write(pos_forward);
-    leg_fr.write(pos_backward);
-    leg_fl.write(pos_forward);
+    leg_bl.write(pos_forward);
+    leg_br.write(pos_backward);
+    leg_fr.write(pos_forward);
+    leg_fl.write(pos_backward);
 
     delay(2500);
   }
 
   int servo_forward(int value){
-    //return static_cast<int>(36.0/19.0 * value);
-    return value;
+    return static_cast<int>(36.0/19.0 * value);
   }
 
   int servo_backward(int value){
-    //return static_cast<int>(-36.0/19.0 * value + 180);
-    return 180 - value;
+    return static_cast<int>(-36.0/19.0 * value + 180);
   }
 
-  void write_leg_bl(int hip, int knee){
-    int pos[] = {servo_backward(knee), servo_forward(hip)};
-    PRINT("Positions: ")
-    PRINT(hip)
-    PRINT(", ")
-    PRINT(knee)
+  void write_leg_bl(float x, float y){
+    int* pos;
+    pos = subtask::get_positions(x, y);
 
-    PRINT("Output for pos[0]: ")
-    PRINT(pos[0])
-
-    PRINT("Output for pos[1]: ")
-    PRINT(pos[1])
-    leg_bl.write(pos);
+    int values[] = {servo_forward(pos[0]), servo_backward(pos[1])};
+    leg_bl.write(values);
   }
 
-  void write_leg_br(int hip, int knee){
-    int pos[] = {servo_forward(knee), servo_backward(hip)};
-    leg_br.write(pos);
+  void write_leg_br(float x, float y){
+    int* pos;
+    pos = subtask::get_positions(x, y);
+
+    int values[] = {servo_backward(pos[0]), servo_forward(pos[1])};
+    leg_br.write(values);
   }
 
-  void write_leg_fr(int hip, int knee){
-    int pos[] = {servo_backward(knee), servo_forward(hip)};
-    leg_fr.write(pos);
+  void write_leg_fr(float x, float y){
+    int* pos;
+    pos = subtask::get_positions(-x, y);
+
+    int values[] = {servo_forward(pos[0]), servo_backward(pos[1])};
+    leg_fr.write(values);
   }
 
-  void write_leg_fl(int hip, int knee){
-    int pos[] = {servo_forward(knee), servo_backward(hip)};
-    leg_fl.write(pos);
+  void write_leg_fl(float x, float y){
+    int* pos;
+    pos = subtask::get_positions(-x, y);
+
+    int values[] = {servo_backward(pos[0]), servo_forward(pos[1])};
+    leg_fl.write(values);
   }
 
 }  // namespace actuators
