91d76b0e643cc88e6d8e99460d7406d2236a2047
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-05-15 23:20:03 +0100

    Use of new gait

--- PICO_Robotic_Dog/task.h
+++ PICO_Robotic_Dog/task.h
@@ -6,12 +6,15 @@
 #include "utility.h"
 #include "sidekick.h"
 
+#define INTERP 64
+#define GAIT -3, 3, 14, 10, INTERP
+
 namespace task
 {
-  subtask::Gait leg_br(0);
-  subtask::Gait leg_fl(1);
-  subtask::Gait leg_bl(2);
-  subtask::Gait leg_fr(3);
+  subtask::Gait leg_bl(0, GAIT);
+  subtask::Gait leg_fr(INTERP * 1 / 4, GAIT);
+  subtask::Gait leg_br(INTERP * 2 / 4, GAIT);
+  subtask::Gait leg_fl(INTERP * 3 / 4, GAIT);
 
   void Setup()
   {
@@ -37,11 +40,17 @@ namespace task
     leg_fr.update_forward();
     leg_fl.update_forward();
 
+    PRINT(" INDEX: ");
+    PRINT(leg_bl.get_indx());
+    PRINT(" X: ");
+    PRINT(leg_bl.get_x());
+    PRINT(" Y: ");
+    PRINT(leg_bl.get_y());
+
     actuators::write_leg_bl(leg_bl.get_x(), leg_bl.get_y());
     actuators::write_leg_br(leg_br.get_x(), leg_br.get_y());
     actuators::write_leg_fl(leg_fl.get_x(), leg_fl.get_y());
     actuators::write_leg_fr(leg_fr.get_x(), leg_fr.get_y());
-
-    delay(250);
+    delay(50);
   }
 }
