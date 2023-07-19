229042dd21c3019db3ae992ec39aa7e122f34fa4
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-07-20 00:09:34 +0100

    Added BMI088

--- PICO_Robotic_Dog_Test/sensors.h
+++ PICO_Robotic_Dog_Test/sensors.h
@@ -5,10 +5,62 @@
 //-----Internals------
 
 // Include your sensor libraries here
+#include "BMI088.h"
+#include "vector.h"
+
+#define G 9.81
 
 namespace sensors {
-void init() {}
 
-void update() {}
+  BMI088 bmi088(0x18, 0x68);
 
+  Vec gyro_offset = {0.0f, 0.0f, 0.0f};
+  Vec accel_offset = {0.0f, 0.0f, 0.0f};
+
+  float accel_ratio = G;
+
+  void init() {
+    Wire.begin();
+
+    while (1) {
+      if (bmi088.isConnection()) {
+        bmi088.initialize();
+        PRINT("BMI088 is connected");
+        break;
+      } else {
+        PRINT("BMI088 is not connected");
+        END_LOG;
+      }
+    }
+  }
+
+  Vec get_accel() {
+    Vec acc;
+    bmi088.getAcceleration(&acc.x, &acc.y, &acc.z);
+
+    return acc * accel_ratio;
+  }
+
+  Vec get_gyro() {
+    Vec gyro;
+    bmi088.getGyroscope(&gyro.x, &gyro.y, &gyro.z);
+
+    gyro.x += gyro_offset.x;
+    gyro.y += gyro_offset.y;
+    gyro.z += gyro_offset.z;
+
+    return gyro * PI / 180.0f;
+  }
+
+  Vec get_raw_accel() {
+    Vec acc;
+    bmi088.getAcceleration(&acc.x, &acc.y, &acc.z);
+    return acc;
+  }
+
+  Vec get_raw_gyro() {
+    Vec gyro;
+    bmi088.getGyroscope(&gyro.x, &gyro.y, &gyro.z);
+    return gyro;
+  }
 }  // namespace sensors
