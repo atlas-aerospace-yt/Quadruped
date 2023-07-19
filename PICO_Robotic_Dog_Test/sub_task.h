15589db66abef4db09ccb6d3be569d6dfe6b1874
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-07-20 00:09:25 +0100

    Orientation Calculations

--- PICO_Robotic_Dog_Test/sub_task.h
+++ PICO_Robotic_Dog_Test/sub_task.h
@@ -2,7 +2,13 @@
 
 #include "utility.h"
 
+// Other libraries
+#include "vector.h"
+#include "quaternion.h"
+
 #define LENGTH 7.5f
+
+#define WEIGHT 0.75
 
 namespace subtask {
 
@@ -13,7 +19,6 @@ namespace subtask {
   *
   */
 
-  // mathematical functions for leg position
   float get_alpha(float x, float y)
   {
     float length_new = 0.5f * sqrt(sq(x) + sq(y));
@@ -46,13 +51,40 @@ namespace subtask {
     positions[0] = static_cast<int>(get_gamma(alpha + theta) - beta);
     positions[1] = static_cast<int>(alpha + theta);
 
-    /*PRINT(" Alpha: ");
-    PRINT(alpha);
-    PRINT(" Beta: ");
-    PRINT(get_beta(alpha));
-    PRINT(" Theta: ");
-    PRINT(theta);*/
-
     return positions;
+  }
+
+  /*
+  *
+  * Orientation calculations are in this section of the
+  * code.
+  *
+  */
+
+  Quat ori_quat = {1.0f, 0.0f, 0.0f, 0.0f};
+  Quat gyro_ori = {1.0f, 0.0f, 0.0f, 0.0f};
+
+  Quat get_ori_from_accel(Vec accel){
+    // Estimates orientation from acceleration
+    // https://uk.mathworks.com/matlabcentral/answers/160681-calculating-dual-quaternion-from-two-vectors
+    Vec glob_acc = {0, 0, 9.81};
+    Vec u = accel * glob_acc / (accel * glob_acc).magnitude();
+    float alpha = acos(accel.dot(glob_acc) / (accel.magnitude() * glob_acc.magnitude()));
+    Quat q = {cos(alpha/2), sin(alpha/2)*u.x, sin(alpha/2)*u.y, sin(alpha/2) * u.z};
+    q.normalize();
+
+    return q;
+  }
+
+  void update_ori(Vec gyro, Vec accel, float dt){
+    // Fuses the orientation from accel and gyro
+    // https://courses.cs.washington.edu/courses/cse466/14au/labs/l4/madgwick_internal_report.pdf
+    Quat acc_ori = get_ori_from_accel(accel);
+
+    Quat gyro_rate = {0.0f, gyro.x, gyro.y, gyro.z};
+    gyro_ori += gyro_ori * 0.5f * gyro_rate * dt;
+    gyro_ori.normalize();
+
+    ori_quat = gyro_ori * WEIGHT + acc_ori * (1-WEIGHT);
   }
 }
